74b593b8fbf78a5a755e2496850e610dd55bf936
Anya <anyascheinman@gmail.com>
2023-02-28 16:45:52 -0500

    Movement

--- Assets/Scripts01/Player/PlayerMove.cs
+++ Assets/Scripts01/Player/PlayerMove.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = -3;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
     }
 }
