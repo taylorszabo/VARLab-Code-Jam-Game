262630a500a6596ffcb5893cbf2ae3c5948dffa7
thesomeoldguy <102364953+thesomeoldguy@users.noreply.github.com>
2023-03-01 14:38:34 -0500

    commit 6.0

---
+++ Assets/Scripts/Collectables/CollectableControl.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CollectableControl : MonoBehaviour
+{
+    public static int coinCount;
+    public GameObject coinCountDisplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+        coinCountDisplay.GetComponent<TextMeshProUGUI>().text = "" + coinCount;
+    }
+}
