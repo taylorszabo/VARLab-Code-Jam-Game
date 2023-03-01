8b2d4725fefb00ce553a22fb822dbfa83088e98f
AngelAvilesSil <aavilessil1750@conestogac.on.ca>
2023-03-01 13:33:38 -0500

    Moves in a limited way to sides

--- Assets/EndlessRun/Scripts/Player/PlayerMove.cs
+++ Assets/EndlessRun/Scripts/Player/PlayerMove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed = 3;
+    [SerializeField] public float moveSpeed = 3f;
+    [SerializeField] public float leftRightSpeed = 4f;
     
     // Start is called before the first frame update
     //void Start()
@@ -16,5 +17,20 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
+            {
+				transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
+			}
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
+            {
+				transform.Translate(Vector3.right * Time.deltaTime * leftRightSpeed);
+			}
+        }
     }
 }
