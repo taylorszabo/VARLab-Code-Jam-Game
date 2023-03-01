3b2457d73e1cc8377dd531f9715796e2750d0073
Anya <anyascheinman@gmail.com>
2023-03-01 18:28:23 -0500

    Made generating levels

---
+++ Assets/Scripts01/Envioment/GenerateLevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public GameObject[] sandFloor;
+    public int zPos = 50;
+    public bool creatingSection = false;
+    public int secNum;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!creatingSection)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }
+    }
+
+    IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(sandFloor[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(2);
+        creatingSection = false;
+    }
+}
+
